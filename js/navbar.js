1f08fde4581f925dca860812924046e4dd67a10b
SUKRUCIRIS <kurt12118@gmail.com>
2022-12-31 15:59:11 +0300

    portable navbar js

--- js/navbar.js
+++ js/navbar.js
@@ -1,4 +1,17 @@
-function navbar_hamburger_change(x, y) {
-  x.classList.toggle("change");
-  y.classList.toggle("change");
-}
+document.write(`\
+<script>\
+function navbar_hamburger_change(x, y) {\
+  x.classList.toggle("change");\
+  y.classList.toggle("change");\
+}\
+</script>\
+<nav id="navbar">\
+	<a href="../index.html">Home</a>\
+	<a href="../html/resume.html">Resume</a>\
+	<div class="hamburger" onclick="navbar_hamburger_change(this,document.getElementById('navbar'))">\
+		<div class="bar1"></div>\
+		<div class="bar2"></div>\
+		<div class="bar3"></div>\
+	</div>\
+</nav>\
+`);
