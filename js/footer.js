6e8e6c02e7d15b98475ef7add3d6823995caf4b8
SUKRUCIRIS <kurt12118@gmail.com>
2023-01-01 14:24:33 +0300

    footer added

---
+++ js/footer.js
@@ -0,0 +1,7 @@
+document.write(`
+<footer>
+  <p>For business enquiries: sukruciris2000@gmail.com</p>
+  <p>My LinkedIn profile: linkedin.com/in/sukruciris</p>
+  <p>My YouTube channel: youtube.com/@sukruciris</p>
+</footer>
+`);
